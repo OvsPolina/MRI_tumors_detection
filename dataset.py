8385c95bc24f2d41b0d8efac767dcb71b6dbf324
OvsPolina <58885435+OvsPolina@users.noreply.github.com>
2024-03-13 01:04:49 +0100

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -243,7 +243,7 @@ class CustomDataset:
 
         return image, mask
 
-class CustomDataset_general(Dataset):
+class CustomDataset_brain(Dataset):
     def __init__(self, root_dir, transform=None):
         self.root_dir = root_dir
         self.image_folder = os.path.join(root_dir, "images")
@@ -273,4 +273,34 @@ class CustomDataset_general(Dataset):
             mask = self.transform(mask)
 
         return image_gray, mask
+
+class CustomDataset_liver(Dataset):
+    def __init__(self, root_dir, transform=None):
+        self.root_dir = root_dir
+        self.image_folder = os.path.join(root_dir, "original_images")
+        self.mask_folder = os.path.join(root_dir, "masks")
+        self.image_files = sorted(os.listdir(self.image_folder))
+        self.mask_files = sorted(os.listdir(self.mask_folder))
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.image_files)
+
+    def __getitem__(self, idx):
+        # Read image
+        img_name = self.image_files[idx]
+        img_path = os.path.join(self.image_folder, img_name)
+        image = Image.open(img_path)
+
+        # Read corresponding mask
+        mask_name = self.mask_files[idx]
+        mask_path = os.path.join(self.mask_folder, mask_name)
+        mask = Image.open(mask_path).convert("L")
+
+        if self.transform:
+            # Apply transformations
+            image = self.transform(image)
+            mask = self.transform(mask)
+
+        return image, mask
 
